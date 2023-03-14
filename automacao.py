fa554306fac9c7e1364a118b9b4ab52c02c15192
LucasCPacheco <lucas.pacheco@aluno.faculdadeimpacta.com.br>
2023-03-14 01:52:37 -0300

    Ajuste analise de frames
    Ajuste analise de frames

--- automacao.py
+++ automacao.py
@@ -2,93 +2,62 @@ import funcoespyautogui as pya
 import funcoesanalise as ma
 import funcoesdb as db
 import torch
-#from matplotlib import pyplot as plt
-import numpy as np
-import cv2
-from PIL import ImageGrab
 import time
-import pyautogui
-import os
-import multiprocessing as mp
-from itertools import repeat
 
-print("*************************************************")
-print("Quanto tempo a ferramenta vai ficar executando em minutos:", end = ' ')
-min = int(input())
-sec = min * 60
+if __name__ == '__main__':
 
-print("*************************************************")
-model = torch.hub.load('ultralytics/yolov5', 'yolov5s')
-print("*************************************************")
-print("MODELO CARREGADO")
+    print("*************************************************")
+    print("Tempo de execução da ferramenta (em minutos):", end = ' ')
+    min = int(input())
+    sec = min * 60
 
-pya.abrirTiktok()
-time.sleep(10)
-pya.abrirMidia()
-time.sleep(2)
+    print("Tempo de espera para captar os frames após passar de video (em segundos):", end = ' ')
+    delay = int(input())
 
-print("*************************************************")
-print("MIDIA CARREGADA")
+    print("Tempo de análise de frames dos videos (em segundos):", end = ' ')
+    sec_video = int(input()) + delay
 
-begin_tool = time.perf_counter()
-end_tool = time.perf_counter()
-videos = 0
-while(sec > end_tool - begin_tool):
-
-    pya.passarVideo()
-    time.sleep(2)
-
-    list_frame = []
-    list_results = []
-
-    first_frame = ma.get_frame(bbox=(370,130,870,1020))
-    list_frame.append(first_frame)
-
-    sec_frame = ma.get_frame(bbox=(370,130,870,1020))
-    list_frame.append(sec_frame)
-
-    loop_time = time.time()
-
-    begin_rframe = time.perf_counter()
-    while(True):
-
-        # Vejo 20 segundos de frame, coloquei 22 para descontar os 2 segundos de espera ao passar de video
-        frame = ma.get_frame(bbox=(370,130,870,1020))
-        end_rframe = time.perf_counter()
-        if ((np.array_equiv(frame, first_frame) or np.array_equiv(frame, sec_frame)) or end_rframe - begin_rframe > 22): break
-        else: list_frame.append(frame)
-
-        # # Cria tela com a analise dos frames
-        # frame_color = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        # results = model(frame_color)
-        # imS = cv2.resize(np.squeeze(results.render()), (300, 400))
-        # #imS2 = cv2.resize(frame, (300, 400))
-        # cv2.imshow('YOLO', imS)
-        # if cv2.waitKey(10) & 0xFF == ord('q'):
-        #    break
-        # cv2.destroyAllWindows()
-
-        # # Frames por segundo
-        # print('FPS {}'.format(1 / (time.time() - loop_time)))
-        # loop_time = time.time()
-        
-        # # Tempo de Execução
-        # total = round(end - begin, 2)
-        # print(f'Runtime: {total}')
-
-
-    # Aproximadamente 420 frames em 21 segundos 
-    # Então, 20 frames por segundo
-    # Video tem que ter no minimo 2 segundos relacionado aquele conteudo de animal
-    # Logo, 20x2 = 40 frames com aquele conteudo
-
-    has_animal = ma.analyze_frames(model, list_frame, 40)
-    if (has_animal): time.sleep(3)
-
-    end_tool = time.perf_counter()
-    videos += 1
+    print("*************************************************")
+    num_processes = 4
+    model = torch.hub.load('ultralytics/yolov5', 'yolov5s',  device=0, _verbose=False)
+    model.share_memory()
+    print("*************************************************")
+    print("MODELO CARREGADO NAS GPU")
     
-print("*************************************************")
-print(f'Videos analisados: {videos}')
-pya.fecharMidia()
-db.fecharConexao()
+    pya.abrirTiktok()
+    time.sleep(10)
+    pya.abrirMidia()
+    time.sleep(delay)
+
+    print("*************************************************")
+    print("MIDIA CARREGADA")
+
+    begin_tool = time.perf_counter()
+    end_tool = time.perf_counter()
+    videos = 0
+    while(sec > end_tool - begin_tool):
+
+        video = ma.Video(model, sec_video)
+
+        analise = video.run()
+
+        if (analise.has_animal): 
+            pya.curtirVideo()
+            time.sleep(3)
+        
+        videoURL = pya.copiarLinkVideo()
+        db.salvarVideo(videoURL, len(analise.list_frame), 
+                       analise.apareceCachorro, len(analise.list_conf_dog), analise.avg_conf_dog, 
+                       analise.apareceGato, len(analise.list_conf_cat), analise.avg_conf_cat)
+        videos += 1
+
+        pya.passarVideo()
+        time.sleep(delay)
+
+        end_tool = time.perf_counter()
+        
+    print("*************************************************")
+    print(f'Videos analisados: {videos}')
+
+    pya.fecharMidia()
+    db.fecharConexao()
--- funcoesanalise.py
+++ funcoesanalise.py
@@ -1,53 +1,227 @@
 
 import cv2
 import numpy as np
-import funcoesdb as db
-import funcoespyautogui as pya
 from PIL import ImageGrab
+import time
+import threading
 
-def get_frame(bbox):
-    img = ImageGrab.grab(bbox)
-    frame = np.array(img)
-    return frame
+class Video:
 
-def analyze_frames(model, list_frame, has_qtde_frame):
-    has_dog = 0
-    has_cat = 0
-    list_conf_dog = []
-    list_conf_cat = []
+    def __init__(self, model, sec_video):
+        self.model = model
+        self.sec_video = sec_video
+        self.has_animal = False
+        self.apareceCachorro = 0
+        self.apareceGato = 0
+        self.avg_conf_dog = 0
+        self.avg_conf_cat = 0
+        self.list_frame = []
+        self.has_dog = 0
+        self.list_conf_dog = []
+        self.has_cat = 0
+        self.list_conf_cat = []
+        self.list_threads = []
 
-    for frame in list_frame:
+    def get_frame(self, bbox):
+        img = ImageGrab.grab(bbox)
+        frame = np.array(img)
+        self.list_frame.append(frame)
+        return frame
+    
+    def pred_frame(self, frame):
+        t = threading.Thread(target=self.prediction, args=[frame], daemon=True)
+        self.list_threads.append(t)
+        t.start()
+
+    def wait_threads(self):
+        for thread in self.list_threads:
+            thread.join()
+    
+    def prediction(self, frame):
         frame_color = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        results = model(frame_color)
+        results = self.model(frame_color)
         data_frame = results.pandas().xyxy[0]
         for index in range(len(data_frame)):
             confidence = data_frame.loc[index]['confidence']
             name = data_frame.loc[index]['name']
 
-            if (name == 'dog' and confidence > 0.5): 
-                has_dog +=1
-                list_conf_dog.append(confidence)
+            if (name == 'dog' and confidence >= 0.5):
+                self.has_dog +=1
+                self.list_conf_dog.append(confidence)
 
-            if (name == 'cat' and confidence > 0.5):
-                has_cat +=1
-                list_conf_cat.append(confidence)
+            if (name == 'cat' and confidence >= 0.5):
+                self.has_cat +=1
+                self.list_conf_cat.append(confidence) 
+
+    def run(self):
+
+        begin_rframe = time.perf_counter()
+
+        first_frame = self.get_frame(bbox=(370,130,870,1020))
+        self.pred_frame(first_frame)
+
+        sec_frame = self.get_frame(bbox=(370,130,870,1020))
+        self.pred_frame(sec_frame)
+        
+        while(True):
+            frame = self.get_frame(bbox=(370,130,870,1020))
+            self.pred_frame(sec_frame)
+            end_rframe = time.perf_counter()
+            if (end_rframe - begin_rframe > self.sec_video): 
+                break
+            if (np.array_equiv(frame, first_frame) or np.array_equiv(frame, sec_frame)): 
+                self.sec_video = end_rframe - begin_rframe
+                break
+
+            # # Cria tela com a analise dos frames
+            # frame_color = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            # results = self.model(frame_color)
+            # imS = cv2.resize(np.squeeze(results.render()), (300, 400))
+            # #imS2 = cv2.resize(frame, (300, 400))
+            # cv2.imshow('YOLO', imS)
+            # if cv2.waitKey(10) & 0xFF == ord('q'):
+            #    break
+
+            # # Frames por segundo
+            # print('FPS {}'.format(1 / (time.time() - loop_time)))
+            # loop_time = time.time()
+            
+            # # Tempo de Execução
+            # total = round(end - begin, 2)
+            # print(f'Runtime: {total}')
+
+        self.wait_threads()
+        return self.validate()
+
+    def validate(self):
+
+        # Regra: O video precisa ter pelo menos 2 segundos de cachorro/gato para o conteudo ser de animal domestico
+        # Se em aproximadamente 20 segundos, for pego 420 frames
+        # Então, são 21 frames por segundos
+        # Logo, para o critério de aceitação o conteudo de animal domestico precisa ter 21x2 = 42 frames
+        
+        qtde_frames = len(self.list_frame)
+        fps = round(qtde_frames/self.sec_video)
+        criterio_aceitacao = fps * 2
+
+        has_qtde_frame_dog = self.has_dog >= criterio_aceitacao
+        has_qtde_frame_cat = self.has_cat >= criterio_aceitacao
+
+        if (has_qtde_frame_dog): 
+            self.apareceCachorro = 1
+            self.avg_conf_dog = round((sum(self.list_conf_dog)/len(self.list_conf_dog)),2)
+
+        if (has_qtde_frame_cat): 
+            self.apareceGato = 1
+            self.avg_conf_cat = round((sum(self.list_conf_cat)/len(self.list_conf_cat)),2)
+
+        if (has_qtde_frame_dog and has_qtde_frame_cat
+        or has_qtde_frame_dog and not has_qtde_frame_cat
+        or has_qtde_frame_cat and not has_qtde_frame_dog):
+            self.has_animal = True
+        
+        return self
+
+# def pred_frame(model, frame):
+#     frame_color = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+#     results = model(frame_color)
+#     data_frame = results.pandas().xyxy[0]
+#     for index in range(len(data_frame)):
+#         confidence = data_frame.loc[index]['confidence']
+#         name = data_frame.loc[index]['name']
+
+#         if (name == 'dog' and confidence > 0.5):
+#             global has_dog 
+#             global list_conf_dog 
+#             has_dog +=1
+#             list_conf_dog.append(confidence)
+
+#         if (name == 'cat' and confidence > 0.5):
+#             global has_cat 
+#             global list_conf_cat
+#             has_cat +=1
+#             list_conf_cat.append(confidence)
+
+# async def loop_frames(loop, model, list_frame):
+#     list_task = []
+#     for frame in list_frame:
+#         task = loop.create_task(pred_frame(model, frame))
+#         list_task.append(task)
+#     await asyncio.wait(list_task)
+
+# def analyze_frames(model, list_frame, has_qtde_frame):
+
+#     # begin = time.perf_counter()
+
+#     # with concurrent.futures.ThreadPoolExecutor() as executor:
+#     #     executor.map(pred_frame, repeat(model), list_frame)
+
+#     # # # Tempo de Execução
+#     # end = time.perf_counter()
+#     # total = round(end - begin, 2)
+#     # print(f'Runtime: {total}')
+
+#     # ********************************************************************
+
+#     # begin = time.perf_counter()
+
+#     # loop = asyncio.get_event_loop()
+#     # loop.run_until_complete(loop_frames(loop, model, list_frame))
+
+#     # # # Tempo de Execução
+#     # end = time.perf_counter()
+#     # total = round(end - begin, 2)
+#     # print(f'Runtime: {total}')
+
+#     # ********************************************************************
+
+#     # begin = time.perf_counter()
+
+#     # has_dog = 0
+#     # has_cat = 0
+#     # list_conf_dog = []
+#     # list_conf_cat = []
+
+#     # for frame in list_frame:
+#     #     frame_color = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+#     #     results = model(frame_color)
+#     #     data_frame = results.pandas().xyxy[0]
+#     #     for index in range(len(data_frame)):
+#     #         confidence = data_frame.loc[index]['confidence']
+#     #         name = data_frame.loc[index]['name']
+
+#     #         if (name == 'dog' and confidence > 0.5): 
+#     #             has_dog +=1
+#     #             list_conf_dog.append(confidence)
+
+#     #         if (name == 'cat' and confidence > 0.5):
+#     #             has_cat +=1
+#     #             list_conf_cat.append(confidence)
     
-    videoURL = pya.copiarLinkVideo()
-    has_qtde_frame_dog = has_dog >= has_qtde_frame
-    has_qtde_frame_cat = has_cat >= has_qtde_frame
-    not_has_qtde_frame_dog = has_dog < has_qtde_frame
-    not_has_qtde_frame_cat = has_cat < has_qtde_frame
-    avg_conf_dog = 0
-    avg_conf_cat = 0
-    apareceCachorro = 1 if(has_qtde_frame_dog) else 0
-    apareceGato = 1 if(has_qtde_frame_cat) else 0
-    if (has_qtde_frame_dog): avg_conf_dog = round((sum(list_conf_dog)/len(list_conf_dog)),2)
-    if (has_qtde_frame_cat): avg_conf_cat = round((sum(list_conf_cat)/len(list_conf_cat)),2)
-    db.salvarVideo(videoURL, len(list_frame), apareceCachorro, len(list_conf_dog), avg_conf_dog, apareceGato, len(list_conf_cat), avg_conf_cat)
+#     # # # Tempo de Execução
+#     # end = time.perf_counter()
+#     # total = round(end - begin, 2)
+#     # print(f'Runtime: {total}')
 
-    if (has_qtde_frame_dog and has_qtde_frame_cat
-     or has_qtde_frame_dog and not_has_qtde_frame_cat
-     or has_qtde_frame_cat and not_has_qtde_frame_dog):
-        pya.curtirVideo()
-        return True
-    return False
+#     # ********************************************************************
+    
+#     print(f'Quantidades de frames: {len(list_frame)}')
+#     videoURL = pya.copiarLinkVideo()
+#     has_qtde_frame_dog = has_dog >= has_qtde_frame
+#     has_qtde_frame_cat = has_cat >= has_qtde_frame
+#     not_has_qtde_frame_dog = has_dog < has_qtde_frame
+#     not_has_qtde_frame_cat = has_cat < has_qtde_frame
+#     avg_conf_dog = 0
+#     avg_conf_cat = 0
+#     apareceCachorro = 1 if(has_qtde_frame_dog) else 0
+#     apareceGato = 1 if(has_qtde_frame_cat) else 0
+#     if (has_qtde_frame_dog): avg_conf_dog = round((sum(list_conf_dog)/len(list_conf_dog)),2)
+#     if (has_qtde_frame_cat): avg_conf_cat = round((sum(list_conf_cat)/len(list_conf_cat)),2)
+#     db.salvarVideo(videoURL, len(list_frame), apareceCachorro, len(list_conf_dog), avg_conf_dog, apareceGato, len(list_conf_cat), avg_conf_cat)
+
+#     if (has_qtde_frame_dog and has_qtde_frame_cat
+#      or has_qtde_frame_dog and not_has_qtde_frame_cat
+#      or has_qtde_frame_cat and not_has_qtde_frame_dog):
+#         pya.curtirVideo()
+#         return True
+#     return False
